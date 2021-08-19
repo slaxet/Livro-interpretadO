569dc1a84897f309c5f95cfdd687c3d3166d4082
slaxet <93192913+slaxet@users.noreply.github.com>
2021-08-19 19:45:28 +0000

    add error checking

--- src/BarSymbol.cs
+++ src/BarSymbol.cs
@@ -1,4 +1,6 @@
 /*
  * Copyright (c) 2007-2011 Madhav Vaidyanathan
  *
- *  This program i
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2.
+ 
