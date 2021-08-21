662ff7bdd8ce57f9cb29704a357a06ff9e290a59
slaxet <93192913+slaxet@users.noreply.github.com>
2021-08-21 07:13:16 +0000

    Rename workflow

--- src/BarSymbol.cs
+++ src/BarSymbol.cs
@@ -3,4 +3,6 @@
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2.
- 
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied
