de28e2ba141a9647bde9fa0a2c45b35c1001ce38
slaxet <93192913+slaxet@users.noreply.github.com>
2021-09-15 08:31:47 +0000

    updated some documentation

--- src/BlankSymbol.cs
+++ src/BlankSymbol.cs
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2007-2011 Madhav Vaidyanathan
  *
@@ -20,37 +21,37 @@ using System.Drawing.Drawing2D;
 namespace MidiSheetMusic {
 
 
-/** @class BarSymbol
- * The BarSymbol represents the vertical bars which delimit measures.
- * The starttime of the symbol is the beginning of the new
- * measure.
+/** @class BlankSymbol 
+ * The Blank symbol is a music symbol that doesn't draw anything.  This
+ * symbol is used for alignment purposes, to align notes in different 
+ * staffs which occur at the same time.
  */
-public class BarSymbol : MusicSymbol {
-    private int starttime;
+public class BlankSymbol : MusicSymbol {
+    private int starttime; 
     private int width;
 
-    /** Create a BarSymbol. The starttime should be the beginning of a measure. */
-    public BarSymbol(int starttime) {
+    /** Create a new BlankSymbol with the given starttime and width */
+    public BlankSymbol(int starttime, int width) {
         this.starttime = starttime;
-        width = MinWidth;
+        this.width = width;
     }
 
     /** Get the time (in pulses) this symbol occurs at.
      * This is used to determine the measure this symbol belongs to.
      */
-    public override int StartTime {
+    public override int StartTime { 
         get { return starttime; }
     }
 
     /** Get the minimum width (in pixels) needed to draw this symbol */
-    public override int MinWidth { 
-        get { return 2 * SheetMusic.LineSpace; }
+    public override int MinWidth {
+        get { return 0; }
     }
 
     /** Get/Set the width (in pixels) of this symbol. The width is set
      * in SheetMusic.AlignSymbols() to vertically align symbols.
      */
-    public override int Width {
+    public override int Width { 
         get { return width; }
         set { width = value; }
     }
@@ -59,7 +60,7 @@ public class BarSymbol : MusicSymbol {
      *  to determine the minimum height needed for the staff (Staff.FindBounds).
      */
     public override int AboveStaff { 
-        get { return 0; } 
+        get { return 0; }
     }
 
     /** Get the number of pixels this symbol extends below the staff. Used
@@ -69,21 +70,13 @@ public class BarSymbol : MusicSymbol {
         get { return 0; }
     }
 
-    /** Draw a vertical bar.
+    /** Draw nothing.
      * @param ytop The ylocation (in pixels) where the top of the staff starts.
      */
-    public override 
-    void Draw(Graphics g, Pen pen, int ytop) {
-        int y = ytop;
-        int yend = y + SheetMusic.LineSpace*4 + SheetMusic.LineWidth*4;
-        pen.Width = 1;
-        g.DrawLine(pen, SheetMusic.NoteWidth/2, y, 
-                        SheetMusic.NoteWidth/2, yend);
-
-    }
+    public override void Draw(Graphics g, Pen pen, int ytop) {}
 
     public override string ToString() {
-        return string.Format("BarSymbol starttime={0} width={1}", 
+        return string.Format("BlankSymbol starttime={0} width={1}", 
                              starttime, width);
     }
 }
---
+++ src/ChordSymbol.cs
@@ -0,0 +1,21 @@
+
+/*
+ * Copyright (c) 2007-2011 Madhav Vaidyanathan
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace MidiSheetMusic {
