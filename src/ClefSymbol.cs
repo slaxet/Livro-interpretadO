76ae2df0aeb7d3ee64f0b86a40db68835c5401e1
slaxet <93192913+slaxet@users.noreply.github.com>
2021-10-28 08:02:12 +0000

    Updated Travis

--- src/ClefSymbol.cs
+++ src/ClefSymbol.cs
@@ -7,4 +7,11 @@
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for mor
+ *  GNU General Public License for more details.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawi
