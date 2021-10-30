fd253bcc7938abd93f38231ab71e14a3bc62846d
slaxet <93192913+slaxet@users.noreply.github.com>
2021-10-30 14:56:42 +0000

    improving tests

--- src/ClefSymbol.cs
+++ src/ClefSymbol.cs
@@ -20,4 +20,8 @@ using System.Drawing.Drawing2D;
 namespace MidiSheetMusic {
 
 /** The possible clefs, Treble or Bass */
-public e
+public enum Clef { Treble, Bass };
+
+/** @class ClefSymbol 
+ * A ClefSymbol represents either a Treble or Bass Clef image.
+ * T
