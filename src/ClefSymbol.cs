42bbc1498b71bde34f19a3545b4879f123aafab2
slaxet <93192913+slaxet@users.noreply.github.com>
2021-11-07 16:54:35 +0000

    Whoops...

--- src/ClefSymbol.cs
+++ src/ClefSymbol.cs
@@ -76,4 +76,10 @@ public class ClefSymbol : MusicSymbol {
 
     /** Get/Set the width (in pixels) of this symbol. The width is set
      * in SheetMusic.AlignSymbols() to vertically align symbols.
-     *
+     */
+    public override int Width {
+       get { return width; }
+       set { width = value; }
+    }
+
+    /** G
