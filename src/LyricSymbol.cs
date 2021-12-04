3c5bf0ad84be00231ca4de7db3e6a72a3ef340de
slaxet <93192913+slaxet@users.noreply.github.com>
2021-12-04 22:53:50 +0000

    require fix

--- src/LyricSymbol.cs
+++ src/LyricSymbol.cs
@@ -4,4 +4,6 @@
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2.
  *
- *  This program is distributed in the hope that it will be us
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PA
