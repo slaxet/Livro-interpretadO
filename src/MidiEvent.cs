974215013a8b7f34515dc17bf76c3008eb0faa60
slaxet <93192913+slaxet@users.noreply.github.com>
2022-01-01 14:12:58 +0000

    chore: Bump version

--- src/MidiEvent.cs
+++ src/MidiEvent.cs
@@ -5,4 +5,6 @@
  *  it under the terms of the GNU General Public License version 2.
  *
  *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the impl
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more d
