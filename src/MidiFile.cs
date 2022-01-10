0468f2789d47bd8f670d210a03d605795fc93d0e
slaxet <93192913+slaxet@users.noreply.github.com>
2022-01-10 10:27:20 +0000

    sock_diag: Fix out-of-bounds access to sock_diag_handlers[]

--- src/MidiFile.cs
+++ src/MidiFile.cs
@@ -204,3 +204,138 @@ public class MidiFile {
     public const int EventNoteOff         = 0x80;
     public const int EventNoteOn          = 0x90;
     public const int EventKeyPressure     = 0xA0;
+    public const int EventControlChange   = 0xB0;
+    public const int EventProgramChange   = 0xC0;
+    public const int EventChannelPressure = 0xD0;
+    public const int EventPitchBend       = 0xE0;
+    public const int SysexEvent1          = 0xF0;
+    public const int SysexEvent2          = 0xF7;
+    public const int MetaEvent            = 0xFF;
+
+    /* The list of Meta Events */
+    public const int MetaEventSequence      = 0x0;
+    public const int MetaEventText          = 0x1;
+    public const int MetaEventCopyright     = 0x2;
+    public const int MetaEventSequenceName  = 0x3;
+    public const int MetaEventInstrument    = 0x4;
+    public const int MetaEventLyric         = 0x5;
+    public const int MetaEventMarker        = 0x6;
+    public const int MetaEventEndOfTrack    = 0x2F;
+    public const int MetaEventTempo         = 0x51;
+    public const int MetaEventSMPTEOffset   = 0x54;
+    public const int MetaEventTimeSignature = 0x58;
+    public const int MetaEventKeySignature  = 0x59;
+
+    /* The Program Change event gives the instrument that should
+     * be used for a particular channel.  The following table
+     * maps each instrument number (0 thru 128) to an instrument
+     * name.
+     */
+    public static string[] Instruments = {
+
+        "Acoustic Grand Piano",
+        "Bright Acoustic Piano",
+        "Electric Grand Piano",
+        "Honky-tonk Piano",
+        "Electric Piano 1",
+        "Electric Piano 2",
+        "Harpsichord",
+        "Clavi",
+        "Celesta",
+        "Glockenspiel",
+        "Music Box",
+        "Vibraphone",
+        "Marimba",
+        "Xylophone",
+        "Tubular Bells",
+        "Dulcimer",
+        "Drawbar Organ",
+        "Percussive Organ",
+        "Rock Organ",
+        "Church Organ",
+        "Reed Organ",
+        "Accordion",
+        "Harmonica",
+        "Tango Accordion",
+        "Acoustic Guitar (nylon)",
+        "Acoustic Guitar (steel)",
+        "Electric Guitar (jazz)",
+        "Electric Guitar (clean)",
+        "Electric Guitar (muted)",
+        "Overdriven Guitar",
+        "Distortion Guitar",
+        "Guitar harmonics",
+        "Acoustic Bass",
+        "Electric Bass (finger)",
+        "Electric Bass (pick)",
+        "Fretless Bass",
+        "Slap Bass 1",
+        "Slap Bass 2",
+        "Synth Bass 1",
+        "Synth Bass 2",
+        "Violin",
+        "Viola",
+        "Cello",
+        "Contrabass",
+        "Tremolo Strings",
+        "Pizzicato Strings",
+        "Orchestral Harp",
+        "Timpani",
+        "String Ensemble 1",
+        "String Ensemble 2",
+        "SynthStrings 1",
+        "SynthStrings 2",
+        "Choir Aahs",
+        "Voice Oohs",
+        "Synth Voice",
+        "Orchestra Hit",
+        "Trumpet",
+        "Trombone",
+        "Tuba",
+        "Muted Trumpet",
+        "French Horn",
+        "Brass Section",
+        "SynthBrass 1",
+        "SynthBrass 2",
+        "Soprano Sax",
+        "Alto Sax",
+        "Tenor Sax",
+        "Baritone Sax",
+        "Oboe",
+        "English Horn",
+        "Bassoon",
+        "Clarinet",
+        "Piccolo",
+        "Flute",
+        "Recorder",
+        "Pan Flute",
+        "Blown Bottle",
+        "Shakuhachi",
+        "Whistle",
+        "Ocarina",
+        "Lead 1 (square)",
+        "Lead 2 (sawtooth)",
+        "Lead 3 (calliope)",
+        "Lead 4 (chiff)",
+        "Lead 5 (charang)",
+        "Lead 6 (voice)",
+        "Lead 7 (fifths)",
+        "Lead 8 (bass + lead)",
+        "Pad 1 (new age)",
+        "Pad 2 (warm)",
+        "Pad 3 (polysynth)",
+        "Pad 4 (choir)",
+        "Pad 5 (bowed)",
+        "Pad 6 (metallic)",
+        "Pad 7 (halo)",
+        "Pad 8 (sweep)",
+        "FX 1 (rain)",
+        "FX 2 (soundtrack)",
+        "FX 3 (crystal)",
+        "FX 4 (atmosphere)",
+        "FX 5 (brightness)",
+        "FX 6 (goblins)",
+        "FX 7 (echoes)",
+        "FX 8 (sci-fi)",
+        "Sitar",
+        "Banjo",
