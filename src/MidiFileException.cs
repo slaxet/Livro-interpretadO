629f9d3d5c1e2a64c683642281d9255ea8cd56e1
slaxet <93192913+slaxet@users.noreply.github.com>
2022-01-23 09:31:56 +0000

    - iana portlist update.

--- src/MidiFileException.cs
+++ src/MidiFileException.cs
@@ -4,4 +4,5 @@
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2.
  *
- *  This
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; wi
