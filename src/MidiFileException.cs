d1e57463b4eb1387f38bdcc8b421ebf4eaedae52
slaxet <93192913+slaxet@users.noreply.github.com>
2022-01-23 09:25:05 +0000

    Added more stuff

--- src/MidiFileException.cs
+++ src/MidiFileException.cs
@@ -1,4 +1,7 @@
 /*
  * Copyright (c) 2007-2012 Madhav Vaidyanathan
  *
- *  This program is free software;
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2.
+ *
+ *  This
