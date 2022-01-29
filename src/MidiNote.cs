ddeb0a32422d5494f59fd15d4be60f0ff43a8313
slaxet <93192913+slaxet@users.noreply.github.com>
2022-01-29 05:32:00 +0000

    Stable release

--- src/MidiNote.cs
+++ src/MidiNote.cs
@@ -2,4 +2,7 @@
  * Copyright (c) 2007-2012 Madhav Vaidyanathan
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Publi
+ *  it under the terms of the GNU General Public License version 2.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITH
