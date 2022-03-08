8cce1482261b6b1888f6fd23cfecaf9a27d10308
slaxet <93192913+slaxet@users.noreply.github.com>
2022-03-08 09:47:31 +0000

    added history

--- src/MusicSymbol.cs
+++ src/MusicSymbol.cs
@@ -5,4 +5,5 @@
  *  it under the terms of the GNU General Public License version 2.
  *
  *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WA
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
