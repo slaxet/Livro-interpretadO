21a35d2e72d215dc993f46556ac528764af80b60
slaxet <93192913+slaxet@users.noreply.github.com>
2022-03-10 13:11:20 +0000

    Gerana, nomdefespgrulab

--- src/MusicSymbol.cs
+++ src/MusicSymbol.cs
@@ -7,3 +7,9 @@
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Gen
