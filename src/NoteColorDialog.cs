5ba98c7e5deb85569a48ddc00d7a027d43ced36b
slaxet <93192913+slaxet@users.noreply.github.com>
2022-03-21 18:33:40 +0000

    centering

--- src/NoteColorDialog.cs
+++ src/NoteColorDialog.cs
@@ -1,2 +1,5 @@
 /*
- * Copyright (c) 2007-2011 Madhav Vaidyan
+ * Copyright (c) 2007-2011 Madhav Vaidyanathan
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Publ
