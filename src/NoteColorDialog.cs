31c461c7897963a2c9f09a26d354b78b529dedd2
slaxet <93192913+slaxet@users.noreply.github.com>
2022-03-22 07:54:47 +0000

    Release version 1.0.3

--- src/NoteColorDialog.cs
+++ src/NoteColorDialog.cs
@@ -5,4 +5,5 @@
  *  it under the terms of the GNU General Public License version 2.
  *
  *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
