1e4383d04afe9d7c44524ee3ffa051b08039a374
slaxet <93192913+slaxet@users.noreply.github.com>
2022-07-12 14:26:36 +0000

    Actual initial commit

--- src/NoteColorDialog.cs
+++ src/NoteColorDialog.cs
@@ -153,4 +153,8 @@ public class NoteColorDialog {
         shade2Button.Location = new Point(buttons[11].Location.X, label.Location.Y);
         shade2Button.Size = new Size(maxwidth*3/2, labelheight-4);
         shade2Button.ForeColor = shade2Button.BackColor = shade2Color;
-        shade2Button.Click +
+        shade2Button.Click += new EventHandler(SetNoteColor);
+
+
+        /* Create the OK and Cancel buttons */
+        xstart =
