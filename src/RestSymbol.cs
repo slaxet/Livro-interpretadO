c281487b2c1c618e368dfafeefc1083a9a7648f6
slaxet <93192913+slaxet@users.noreply.github.com>
2022-11-27 12:08:40 +0000

    Changed a query to use parameters

--- src/RestSymbol.cs
+++ src/RestSymbol.cs
@@ -46,4 +46,10 @@ public class RestSymbol : MusicSymbol {
 
     /** Get/Set the width (in pixels) of this symbol. The width is set
      * in SheetMusic.AlignSymbols() to vertically align symbols.
-     *
+     */
+    public override int Width {
+        get { return width; }
+        set { width = value; }
+    }
+
+    /** Get the minimum wid
