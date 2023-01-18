b4f77418bfa95e6aee2473657d63e7b1dbc8785d
slaxet <93192913+slaxet@users.noreply.github.com>
2023-01-18 13:16:50 +0000

    prepare for publish

--- src/RestSymbol.cs
+++ src/RestSymbol.cs
@@ -99,4 +99,7 @@ public class RestSymbol : MusicSymbol {
     }
 
 
-    /** Draw a whole rest symbol, a rectangle below a 
+    /** Draw a whole rest symbol, a rectangle below a staff line.
+     * @param ytop The ylocation (in pixels) where the top of the staff starts.
+     */
+    public void 
